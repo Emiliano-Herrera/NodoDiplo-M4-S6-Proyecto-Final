43e9c321f39816b50f010bccc8db107f7596b357
Emiliano-Herrera <emilianoherrera701@gmail.com>
2026-05-28 19:34:42 -0300

    Sprint 6, practico final - cambios en el arch api V 0.11.

--- diplomatura-fullstack/frontend/src/services/api.js
+++ diplomatura-fullstack/frontend/src/services/api.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
-const api = axios.create({
+/* const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: false,  // Importante para CORS
+}); */
+
+const api = axios.create({
+  baseURL: 'https://diplomatura-backend-u85h.onrender.com', // Hardcodeado
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use(
